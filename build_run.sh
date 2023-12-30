9ae2aab5cb14f75522fd43be326f60d8e558c0e3
qhaud <ddingm1044@gmail.com>
2023-12-31 00:51:51 +0900

    build sh

--- build_run.sh
+++ build_run.sh
@@ -4,4 +4,4 @@ TARGET_CONTAINER=$1
 TARGET_IMAGE="${TARGET_CONTAINER}:latest"
 
 docker build -t $TARGET_IMAGE .
-docker run -d -p 17001:80 --name $TARGET_CONTAINER
+docker run -d -p 17001:80 --name $TARGET_CONTAINER $TARGET_IMAGE

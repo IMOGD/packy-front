9468d0d702b42a5414d73bb97f93998d3e3b21b1
qhaud <ddingm1044@gmail.com>
2023-12-31 11:12:35 +0900

    kill container

--- build_run.sh
+++ build_run.sh
@@ -6,4 +6,10 @@ TARGET_IMAGE="$TARGET_CONTAINER:latest"
 echo $TARGET_CONTAINER
 
 docker build -t $TARGET_IMAGE .
+
+set +
+docker kill $TARGET_CONTAINER
+docker rm $TARGET_CONTAINER
 docker run -d -p 17001:80 --name $TARGET_CONTAINER $TARGET_IMAGE
+
+set -
